9fdbb2b71f70f45e59eb44e33f2db9c173e49c10
Aniket Sedhai <asedhai@masonite.com>
2024-07-11 22:06:20 -0500

    Initial Commit

---
+++ Checkpoint03/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Checkpoint03
+//
+//  Created by Aniket Sedhai on 7/11/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
